2846d86ec6777ff435134725ea47aa347940c976
zozep <jpark041@gmail.com>
2019-09-16 15:18:55 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.floor( Math.random()* 10 ) + 1;
+// console.log(random)
+const mod = 16 % 6
+const max = Math.max( 5, 10, 15, 20 )
+
+// const max = function() {
+//     let set = [1,2,3,20];
+//     let currentMax = set[0];
+
+//     for (let indexVal of set) {
+//         if (indexVal > currentMax) {
+//             currentMax = indexVal;
+//         }
+//         // console.log(max);
+//     }
+//     console.log("this is max: " + currentMax);
+//     // currentMax = set[3];
+//     return currentMax;
+// };
+
+// max();
